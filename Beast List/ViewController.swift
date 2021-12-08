ce02095d262b3b71ac6e269985c8c33d7d256d82
Lubabah-mujallid <90623112+Lubabah-mujallid@users.noreply.github.com>
2021-12-08 18:01:38 +0000

    ++

--- Beast List/ViewController.swift
+++ Beast List/ViewController.swift
@@ -1,19 +1,31 @@
-//
-//  ViewController.swift
-//  Beast List
-//
-//  Created by admin on 08/12/2021.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tfTask: UITextField!
+    var tasks = ["clean room","read book","eat healthy food"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
+    @IBAction func addButtonPressed(_ sender: Any) {
+        if let task = tfTask.text {
+            tasks.append(task)
+            tableView.reloadData()
+            tfTask.text = ""
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row] 
+        return cell
+    }
+}
